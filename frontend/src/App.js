269915d9aa82977f869aeafc1ce25b7f2815692a
Utahachi <philipvergara0524@gmail.com>
2026-02-25 23:05:43 +0800

    feature(frontend): Created a Protected Route Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
