eb2fc5c89611385bc0e994823c4a7228a802a89e
Utahachi <philipvergara0524@gmail.com>
2026-02-25 22:56:58 +0800

    feature: Setup Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
