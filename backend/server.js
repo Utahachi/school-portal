ef7bb8c9fed7c5f3eb76f6cf4e5a234131fbd60b
Utahachi <philipvergara0524@gmail.com>
2026-02-25 22:11:28 +0800

    feature(backend): Initialized db.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const pool = require("./db");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("School Portal API Running");
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database connection failed");
+  }
 });
 
 app.listen(5000, () => {
