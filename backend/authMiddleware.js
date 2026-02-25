72326e4f3cc399769dd8fcd50ad62ef15ba6b188
Utahachi <philipvergara0524@gmail.com>
2026-02-25 22:29:52 +0800

    feature(backend): Created Auth Middleware

---
+++ backend/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = function (req, res, next) {
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).send("No token provided");
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).send("Invalid token");
+  }
+};
