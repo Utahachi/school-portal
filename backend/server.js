0cdaa819b3ea313066d0ac686b4699dafa6b5774
Utahachi <philipvergara0524@gmail.com>
2026-02-25 22:17:24 +0800

    feature(backend): Added a POST User Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,19 @@ const pool = require("./db");
 
 const app = express();
 
+app.use(express.json());
+
+// Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching users");
+  }
+});
+
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
