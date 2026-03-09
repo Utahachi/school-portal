7743a6587bea32d9e0befdb2acf352e28b7f3f77
Utahachi <philipvergara0524@gmail.com>
2026-03-09 20:50:31 +0800

    feat(frontend): Updated ProtectedRoute with roles to not access teacher's dashboard as students and vice versa

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,13 +1,25 @@
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute({ children, role }) {
   const token = localStorage.getItem("accessToken");
 
   if (!token) {
     return <Navigate to="/" />;
   }
 
-  return children;
+  try {
+    const decoded = jwtDecode(token);
+
+    if (role && decoded.role !== role) {
+      return <Navigate to="/" />;
+    }
+
+    return children;
+
+  } catch (err) {
+    return <Navigate to="/" />;
+  }
 }
 
 export default ProtectedRoute;
