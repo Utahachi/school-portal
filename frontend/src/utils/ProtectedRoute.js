f7a3fcb923f5afe51999a3ad751427147bb5e5dd
Utahachi <philipvergara0524@gmail.com>
2026-03-09 20:46:34 +0800

    refactor: removed /login in navigation and changed from token to accessToken

--- frontend/src/utils/ProtectedRoute.js
+++ frontend/src/utils/ProtectedRoute.js
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("accessToken");
 
   if (!token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/" />;
   }
 
   return children;
